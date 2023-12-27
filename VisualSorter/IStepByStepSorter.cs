c9aacb0dda8268a71552f931d3932b115aca575b
avrohomthousman <avrohomthousman@gmail.com>
2023-12-27 17:24:01 -0500

    Made interface for sorting algorithm

---
+++ VisualSorter/IStepByStepSorter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualSorter
+{
+    /// <summary>
+    /// Sorts an array of integers one step at a time, returning each swap as it is done.
+    /// </summary>
+    public interface IStepByStepSorter
+    {
+
+        /// <summary>
+        /// Executes the sort
+        /// </summary>
+        /// <returns>the two indexes that are swapped at each step of the sort</returns>
+        public IEnumerable<Tuple<int, int>> Sort();
+
+    }
+}
