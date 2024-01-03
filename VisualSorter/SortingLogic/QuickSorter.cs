3cdbd2a4e8404f82bfa877076b6b55b8380df126
avrohomthousman <avrohomthousman@gmail.com>
2024-01-02 21:11:15 -0500

    started setting up a quicksort

--- VisualSorter/SortingLogic/QuickSorter.cs
+++ VisualSorter/SortingLogic/QuickSorter.cs
@@ -22,5 +22,85 @@ namespace VisualSorter.SortingLogic
         {
             throw new NotImplementedException();
         }
+
+
+
+        /// <summary>
+        /// Does one partiotion of the quicksort.
+        /// </summary>
+        /// <param name="start">the start index of the area being sorted</param>
+        /// <param name="end">the end index of the area being sorted</param>
+        private void Partition(int start, int end)
+        {
+            if(end - start <= 1)
+            {
+                return;
+            }
+            if(end - start == 2)
+            {
+                //TODO swap if needed, then return
+            }
+
+            int pivotIndex = ChoosePivot(start, end);
+
+
+            //TODO: normal sort
+        }
+
+
+
+
+        /// <summary>
+        /// Chooses a good pivot to be used when partioning this part of the array
+        /// </summary>
+        /// <param name="start">the start index of the area being sorted</param>
+        /// <param name="end">the end index of the area being sorted</param>
+        /// <returns>a good pivot to use</returns>
+        private int ChoosePivot(int start, int end)
+        {
+            int first = data[data.Length - 1];
+            int second = data[data.Length - 2];
+            int third = data[data.Length - 3];
+
+
+            if(IsBetween(first, second, third))
+            {
+                return data.Length - 1;
+            }
+            if(IsBetween(second, first, third))
+            {
+                return data.Length - 2;
+            }
+
+            return data.Length - 3;
+        }
+
+
+
+
+        /// <summary>
+        /// Checks if the target number is between the other two numbers. The first does not need
+        /// to be larger than the second number.
+        /// </summary>
+        /// <param name="target">the number that is being checked</param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>true if the target is between the other two and false otherwise</returns>
+        private bool IsBetween(int target, int first, int second)
+        {
+            int larger, smaller;
+            if(first > second)
+            {
+                larger = first;
+                smaller = second;
+            }
+            else
+            {
+                larger = second;
+                smaller = first;
+            }
+
+            return target >= smaller && target <= larger;
+        }
     }
 }
